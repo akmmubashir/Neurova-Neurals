82f879f25313862c9b9e37eba302355e862aa878
akmmubashir <akmmubashir@gmail.com>
2025-12-17 15:21:09 +0530

    why choose us added

--- app/components/whyChooseUs.tsx
+++ app/components/whyChooseUs.tsx
@@ -1,15 +1,37 @@
+import Image from 'next/image'
 import React from 'react'
-
+import { whyChooseUsFeatures } from '../utils/data'
 
 const WhyChooseUs = () => {
     return (
-        <div className='max-w-300 mx-auto max-xl:p-[60px_40px_140px] max-lg:p-[50px_20px_120px] grid grid-cols-12 gap-5'>
+        <div className='max-w-300 mx-auto p-[0_0_80px] max-xl:p-[0_60px_40px] max-lg:p-[0_20px_40px] grid grid-cols-12 items-center gap-10 max-lg:gap-[30px_0]'>
             <div className='col-span-6 max-lg:col-span-full'>
-                <h2 className='mb-1 text-[#2a2a2a] font-medium text-[46px] max-2xl:text-[40px] max-xl:text-[36px] max-lg:text-[32px] max-md:text-[28px]'>Where Education Meets Innovation</h2>
+                <h2 className='mb-2 text-[#2a2a2a] font-semibold leading-[1.2] text-[46px] max-2xl:text-[40px] max-xl:text-[36px] max-lg:text-[32px] max-md:text-[28px]'>Where Education Meets <span className="text-[#EA4434]">
+                    Innovation
+                </span>
+                </h2>
                 <p className='text-[#3a3a3a] text-[16px] max-lg:text-[14px]'>Explore our diverse range of courses designed to empower learners of all ages. From foundational subjects to advanced topics, our curriculum is crafted to foster knowledge, creativity, and critical thinking. Join us on a journey of discovery and skill-building that prepares you for the future.
                 </p>
             </div>
-            <div className='col-span-6 max-lg:col-span-full'>
+            <div className='col-span-6 max-lg:col-span-full grid grid-cols-12 gap-5 max-lg:gap-4'>
+                {whyChooseUsFeatures.map(feature =>
+                    <div key={feature.id} className='flex flex-col justify-center gap-2 col-span-6 p-5 max-lg:p-3.75 bg-white shadow-xl rounded-2xl'>
+                        <div className='w-18 max-lg:w-12'>
+                            <Image
+                                src={feature.icon}
+                                alt={feature.title}
+                                width={600}
+                                height={400}
+                                className='w-full h-auto'
+                            />
+                        </div>
+                        <h6 className='text-[#6a6a6a] font-medium text-[16px] max-lg:text-[14px]'>
+                            <span className="text-[20px] max-lg:text-[18px] align-start text-[#2a2a2a]">{feature.title.charAt(0)}</span><span>
+                                {feature.title.slice(1)}
+                            </span>
+                        </h6>
+                    </div>
+                )}
             </div>
         </div>
     )
--- app/utils/data.ts
+++ app/utils/data.ts
@@ -4,23 +4,51 @@ export const courses = [
 		title: 'Junior Tech Explorers',
 		ageGroup: '6-8 years',
 		content:
-        'An introductory course designed to help young learners explore technology through fun activities, storytelling, and hands-on learning. Children develop basic digital awareness, logical thinking, and creativity using age-appropriate tools.',
-        image: '/assets/course/course1.png',
+			'An introductory course designed to help young learners explore technology through fun activities, storytelling, and hands-on learning. Children develop basic digital awareness, logical thinking, and creativity using age-appropriate tools.',
+		image: '/assets/course/course1.png',
 	},
 	{
-        id: 2,
+		id: 2,
 		title: 'Creative Coding for Kids',
 		ageGroup: '8-10 years',
 		content:
-        'This course introduces coding fundamentals using visual and block-based programming. Kids learn to build simple games, animations, and stories while strengthening problem-solving and logical reasoning skills.',
-        image: '/assets/course/course2.png',
+			'This course introduces coding fundamentals using visual and block-based programming. Kids learn to build simple games, animations, and stories while strengthening problem-solving and logical reasoning skills.',
+		image: '/assets/course/course2.png',
 	},
 	{
-        id: 3,
+		id: 3,
 		title: 'Smart Robotics & AI Basics',
 		ageGroup: '8-10 years',
 		content:
-        'A beginner-friendly introduction to robotics and artificial intelligence. Students explore how robots work, understand basic automation concepts, and learn how AI is used in everyday life through interactive projects.',
-        image: '/assets/course/course3.png',
+			'A beginner-friendly introduction to robotics and artificial intelligence. Students explore how robots work, understand basic automation concepts, and learn how AI is used in everyday life through interactive projects.',
+		image: '/assets/course/course3.png',
+	},
+];
+
+export const whyChooseUsFeatures = [
+	{
+		id: 1,
+		title: 'Age-based structured learning paths',
+		icon: '/assets/icons/bullseye.svg',
+	},
+	{
+		id: 2,
+		title: 'Focus on creativity, logic, and problem-solving',
+		icon: '/assets/icons/brain.svg',
+	},
+	{
+		id: 3,
+		title: 'Hands-on exposure to coding, AI, and robotics',
+		icon: '/assets/icons/robot.svg',
+	},
+	{
+		id: 4,
+		title: 'Safe, child-friendly digital environment',
+		icon: '/assets/icons/safe.svg',
+	},
+	{
+		id: 5,
+		title: 'Trusted by schools and educators',
+		icon: '/assets/icons/educator.svg',
 	},
 ];
